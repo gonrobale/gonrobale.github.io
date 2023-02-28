e3f847f377b37c28a975fdefed2474aee3449231
gonrobale <123420140+gonrobale@users.noreply.github.com>
2023-02-28 11:50:37 -0300

    Update funcionalidad.js

--- funcionalidad.js
+++ funcionalidad.js
@@ -1,4 +1,13 @@
 document.getElementById('fondo-oscuro').addEventListener('click', function() {
 	document.body.style.background = 'rgb(10, 100, 150)';
-	document.body.style.color = 'rgb(255, 255, 255)'
+	document.body.style.color = 'rgb(255, 255, 255)';
+	document.getElementById('fondo-oscuro').style.display = 'none';
+	document.getElementById('fondo-claro').style.display = 'block';
+})
+
+document.getElementById('fondo-claro').addEventListener('click', function() {
+	document.body.style.background = 'rgb(200, 200, 200)';
+	document.body.style.color = 'black';
+	document.getElementById('fondo-oscuro').style.display = 'block';
+	document.getElementById('fondo-claro').style.display = 'none';
 })
