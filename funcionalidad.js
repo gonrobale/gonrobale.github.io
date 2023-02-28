efc319063380bb9a8bac6bedce261cd35a732678
gonrobale <123420140+gonrobale@users.noreply.github.com>
2023-02-28 01:18:05 -0300

    Add files via upload

---
+++ funcionalidad.js
@@ -0,0 +1,4 @@
+document.getElementById('fondo-oscuro').addEventListener('click', function() {
+	document.body.style.background = 'rgb(10, 100, 150)';
+	document.body.style.color = 'rgb(255, 255, 255)'
+})
