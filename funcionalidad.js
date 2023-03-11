7da71351a826314b69be77a25997f09790f508f0
gonrobale <123420140+gonrobale@users.noreply.github.com>
2023-03-10 21:57:44 -0300

    Update funcionalidad.js

--- funcionalidad.js
+++ funcionalidad.js
@@ -1,13 +1,26 @@
-document.getElementById('fondo-oscuro').addEventListener('click', function() {
-	document.body.style.background = 'rgb(10, 100, 150)';
-	document.body.style.color = 'rgb(255, 255, 255)';
-	document.getElementById('fondo-oscuro').style.display = 'none';
-	document.getElementById('fondo-claro').style.display = 'block';
-})
+let estilo_claro = true;
 
-document.getElementById('fondo-claro').addEventListener('click', function() {
-	document.body.style.background = 'rgb(200, 200, 200)';
-	document.body.style.color = 'black';
-	document.getElementById('fondo-oscuro').style.display = 'block';
-	document.getElementById('fondo-claro').style.display = 'none';
-})
+document.getElementById('fondo-oscuro').addEventListener('click', function () {
+		
+
+		if (estilo_claro) {
+			document.body.style.background = 'rgb(10, 100, 150)';
+			document.body.style.color = 'rgb(255, 255, 255)';
+			document.getElementById('fondo-oscuro').style.background ='white';
+			estilo_claro = false;
+			console.log(estilo_claro)
+		}
+
+		
+
+		else  {
+			document.body.style.background = 'rgb(200, 200, 200)';
+			document.body.style.color = 'black';
+			document.getElementById('fondo-oscuro').style.background = 'skyblue';
+			estilo_claro = true;
+			console.log(estilo_claro)
+		}
+
+	}
+
+)
